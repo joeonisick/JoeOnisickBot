ac9c1e2e483572ce82935741247726daf29ea2bd
Joe Onisick <joe@definethecloud.net>
2022-08-04 19:44:35 -0600

    Broke code out into functions. Added a while loop timer for main. Added kludge shit code for replying to new tweets. (only functions when @JoeOnisickBot is the first thing in the tweet.

--- App.py
+++ App.py
@@ -1,4 +1,5 @@
 import tweepy
+import time
 from secrets import declare_secrets
 
 #provide keys
@@ -10,54 +11,66 @@ bearer_token, consumer_key,consumer_secret,access_token,access_token_secret \
 
 #Configure the client for use of OAuth1.0 and OAuth2.0 (API object dependant)
 #https://developer.twitter.com/en/docs/authentication/guides/v2-authentication-mapping
-client = tweepy.Client( 
-    bearer_token = bearer_token, consumer_key=consumer_key, 
-    consumer_secret=consumer_secret, access_token=access_token, 
-    access_token_secret=access_token_secret, wait_on_rate_limit= True
-) 
 
-#Screen name to look for
-screen_name = "JoeOnisickBot" #The Bots Twitter handle
-user = client.get_users(usernames=screen_name) #Uses handle to retrieve user info
-user_id = user.data[0].id #isolates the unique ID for the user as a var
-#retrieve JoeOnisickBot mentions and their author
-mentions = client.get_users_mentions(id=user_id,expansions='author_id',max_results=25)
+def get_user(client, screen_name):
+    user = client.get_users(usernames=screen_name) #Uses handle to retrieve user info
+    user_id = user.data[0].id #isolates the unique ID for the user as a var
+    return(user, user_id)
 
-#create a list of user_ids Joe Bot follows
-following = client.get_users_following(user_id,max_results=1000)
-followed = []
-#print(following)
-for i in range(0, following.meta['result_count']):
-    followed.append(following.data[i].id)
+def follow_mention(client, user_id, mentions):
+    #create a list of user_ids Joe Bot follows
+    following = client.get_users_following(user_id,max_results=1000)
+    followed = []
+    for i in range(0, following.meta['result_count']):
+        followed.append(following.data[i].id)
 
-# #Follow mentions if not already following
-list_count = 0 #index var
-for user in mentions.includes['users']:
-    print(mentions.includes['users'][list_count].username)
-    print((mentions.includes['users'][list_count].id) not in followed)
-    if (mentions.includes['users'][list_count].id) not in followed:
-        client.follow_user(mentions.includes['users'][list_count].id) #follow the user
-    list_count += 1
+    #Follow mentions if not already following
+    list_count = 0 #index var
+    for user in mentions.includes['users']:
+        if (mentions.includes['users'][list_count].id) not in followed:
+            client.follow_user(mentions.includes['users'][list_count].id) 
+        list_count += 1
 
-#Tweet something
-# print("@%s" % response.includes['users'][i].username)
-# client.create_tweet(text=\
-#     "Theoretically, I now follow anyone who mentions me. I crave attention. It's loney with only @%s"\
-#          % (response.includes['users'][i].username))
+def check_mentions(client, user_id, since_id):
+    #retrieve JoeOnisickBot mentions and their author
+    mentions = client.get_users_mentions(id=user_id,expansions='author_id',\
+         max_results=100,since_id=since_id)
+    #query2 = 'to:JoeOnisickBot -is:reply' #tweets to bot that aren't replies
+    query1 = "to:JoeOnisickBot"
+    tweets = client.search_recent_tweets(query=query1, expansions =\
+         'author_id', max_results=100)
+    users = {u["id"]: u for u in tweets.includes['users']}
+    for tweet in tweets.data:
+        if users[tweet.author_id]:
+            user = users[tweet.author_id]
+            tweet_id = tweet.id
+            query2 = 'from:JoeOnisickBot in_reply_to_tweet_id:'+str(tweet_id)
+            results = client.search_recent_tweets(query=query2) 
+            if results.meta['result_count'] == 0:
+                tweet_text = \
+                    (" Thanks for engaging. This reply is the current extent of my communication logic."\
+                         % user)
+                client.create_tweet(text=tweet_text,in_reply_to_tweet_id=tweet_id)
+    new_since_id = mentions.meta['oldest_id']
+    follow_mention(client, user_id, mentions)
+    return(new_since_id)
 
+def main():
+    user_id = 1554986957532438535 #set JoeOnisickBot's user id
+    #since ID is set to the first mention of the bot. Since ID is used inclusively.
+    since_id = 1555031447047737345 #used to track mentions on the timeline
+    client = tweepy.Client( 
+        bearer_token = bearer_token, consumer_key=consumer_key, 
+        consumer_secret=consumer_secret, access_token=access_token, 
+        access_token_secret=access_token_secret, wait_on_rate_limit= True
+    ) 
+    
+    count = 1
+    while True:
+        since_id = check_mentions(client, user_id, since_id)
+        print("While Loop Count: %s" % count)
+        time.sleep(120)
+        count += 1
 
-
-#mentions = response.data #Data contians the tweet id and text
-#includes = response.includes #includes contains the expansions (author and id)
-#print("Includes: %s" % response.includes)
-#print(includes['users'])
-#print(includes['users'][0].username)
-#print(includes['users'][0].id)
-#print(mentions)
-#print(client.get_users_followers(user_id)) #retrieve followers for an ID
-#print(client.get_users_following(user_id)) #retrieve accounts followed for an ID
-
-# #print JoeOnisickBot tweets
-# timeline = client.get_users_tweets(user_id)
-# for tweet in timeline.data:
-#     print(tweet.text)
+if __name__ == "__main__":
+    main()
