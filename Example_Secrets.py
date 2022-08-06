011c99a3664802c01b50d365080e5625ff68b6f8
Joe Onisick <joe@definethecloud.net>
2022-08-06 12:28:39 -0600

    Added an example secrets.py

---
+++ Example_Secrets.py
@@ -0,0 +1,19 @@
+def declare_secrets():
+    
+    # Provide keys
+    bearer_token = "<insert your key>"
+    consumer_key = "<insert your key>"
+    consumer_secret = "<insert your key>"
+    access_token = "<insert your key>"
+    access_token_secret = "<insert your key>"
+    
+    # Provide photo locations
+    ranch_photos = "<insert your directoy>"
+    cow_photos = "<insert your directoy>"
+    food_photos = "<insert your directoy>"
+    dog_photos = "<insert your directoy>"
+    joe_lives_photos = "<insert your directoy>"
+
+    return(bearer_token, consumer_key,consumer_secret,access_token, \
+        access_token_secret, ranch_photos, joe_lives_photos, cow_photos,\
+             food_photos, dog_photos)
